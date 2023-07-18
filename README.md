824fe47fb97f1c19cde25d0f201180a423a1efc1
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-07-18 23:36:57 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Project implemented during the https://curso.dev course. It replicates the https://www.tabnews.com.br/ website.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 const Home = () => {
-    return(
-        <h1>Vamo agr vercel</h1>
-    )
-}
+  return <h1>Vamo agr vercel</h1>;
+};
 
 export default Home;
