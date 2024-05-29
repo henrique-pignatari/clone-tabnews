0d63c2ae8024f65a53c595250e1181fd5011fee4
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-05-29 01:24:11 -0300

    tests: database reset on beforeAll

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ const query = async (queryObject) => {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true
-  });
-
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues()
   });
 
   try {
@@ -33,3 +25,13 @@ const query = async (queryObject) => {
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === 'production' ? true : false
+}
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database";
+
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
+beforeAll(clearDatabase)
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST"
