c176cec42fbbebdf471beeb896adfe3a14fa33fa
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2024-06-04 17:52:06 -0300

    feat: adds /migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,25 +1,36 @@
 import migrationRunner from 'node-pg-migrate'
 import { join } from "node:path"
+import database from 'infra/database.js'
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient,
+    dryRun: true,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+  }
+
   if (request.method === 'GET') {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-    })
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === 'POST') {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false
     })
-    return response.status(200).json(migrations);
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end()
